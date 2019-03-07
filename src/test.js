02fb0d130d56367b73b49f755606161f76980a82
kongliangzhong <kongliangzhong@gmail.com>
2019-03-07 18:14:29 +0800

    update

--- src/test.js
+++ src/test.js
@@ -3,6 +3,9 @@ const util = require("util");
 const Tx = require('ethereumjs-tx');
 const pjs = require("protocol2-js");
 const BN = require("bn.js");
+const assert = require('assert');
+global.assert = assert;
+
 const contractAddresses = require("../deployedAddresses.json");
 
 // abi of contract github.com/Loopring/protocol2/contracts/test/DummyToken.sol
@@ -70,6 +73,19 @@ const ringSubmitterAddress = contractAddresses["RingSubmitter"];
 const orderCancellerAddress = contractAddresses["OrderCanceller"];
 /*  --- contracts on ganache end --- */
 
+const context = new pjs.Context(0,
+                                0,
+                                tradeDelegateAddress,
+                                tradeHistoryAddress,
+                                brokerRegistryAddress,
+                                orderRegistryAddress,
+                                feeHolderAddress,
+                                orderBookAddress,
+                                burnRateTableAddress,
+                                LrcAddress,
+                                1000,
+                                0);
+
 async function sendTransaction(from, to, value, data, privKey) {
   const txCount = await web3.eth.getTransactionCount(from);
   const txData = {
@@ -109,7 +125,7 @@ async function getEventsFromContract(contract, eventName, fromBlock) {
 }
 
 async function watchAndPrintEvent(contract, eventName) {
-  const events = await this.getEventsFromContract(contract, eventName, 0);
+  const events = await getEventsFromContract(contract, eventName, 0);
 
   events.forEach((e) => {
     console.log("event:", util.inspect(e.args, false, null));
@@ -135,54 +151,102 @@ function numberToHex(n) {
   return "0x" + n.toString(16);
 }
 
-async function test() {
-  // you may create new account using webjs:
+async function setupOrder(order, index) {
+  if (order.owner === undefined) {
+    throw new Error("order's owner is empty");
+  }
+  if (order.feeAmount === undefined) {
+    order.feeAmount = 1e18;
+  }
+  if (!order.dualAuthSignAlgorithm) {
+    order.dualAuthSignAlgorithm = pjs.SignAlgorithm.Ethereum;
+  }
+
+  if (!order.allOrNone) {
+    order.allOrNone = false;
+  }
+  if (!order.validSince) {
+    // Set the order validSince time to a bit before the current timestamp;
+    const blockNumber = await web3.eth.getBlockNumber();
+    order.validSince = (await web3.eth.getBlock(blockNumber)).timestamp - 1000;
+  }
+  if (!order.validUntil && (order.index % 2) === 1) {
+    // Set the order validUntil time to a bit after the current timestamp;
+    const blockNumber = await web3.eth.getBlockNumber();
+    order.validUntil = (await web3.eth.getBlock(blockNumber)).timestamp + 2500;
+  }
+
+  // if (order.walletAddr === undefined) {
+  //   order.walletAddr = "0x0";
+  // }
+  if (order.walletAddr && order.walletSplitPercentage === undefined) {
+    order.walletSplitPercentage = ((index + 1) * 10) % 100;
+  }
+  if (order.signAlgorithm === undefined) {
+    const signAlgorithmIndex = index % 2;
+    order.signAlgorithm = pjs.SignAlgorithm.EIP712;
+  }
+  if (order.signAlgorithm === pjs.SignAlgorithm.EIP712 && !order.signerPrivateKey) {
+    throw new Error("order owner's privatekey not set");
+  }
+  // Fill in defaults (default, so these will not get serialized)
+  order.version = 0;
+  order.validUntil = order.validUntil ? order.validUntil : 0;
+  order.tokenRecipient = order.tokenRecipient ? order.tokenRecipient : order.owner;
+  order.feeToken = order.feeToken ? order.feeToken : LrcAddress;
+  order.feeAmount = order.feeAmount ? order.feeAmount : 0;
+  order.waiveFeePercentage = order.waiveFeePercentage ? order.waiveFeePercentage : 0;
+  order.tokenSFeePercentage = order.tokenSFeePercentage ? order.tokenSFeePercentage : 0;
+  order.tokenBFeePercentage = order.tokenBFeePercentage ? order.tokenBFeePercentage : 0;
+  order.walletSplitPercentage = order.walletSplitPercentage ? order.walletSplitPercentage : 0;
+  order.tokenTypeS = order.tokenTypeS ? order.tokenTypeS : pjs.TokenType.ERC20;
+  order.tokenTypeB = order.tokenTypeB ? order.tokenTypeB : pjs.TokenType.ERC20;
+  order.tokenTypeFee = order.tokenTypeFee ? order.tokenTypeFee : pjs.TokenType.ERC20;
+  order.trancheS = order.trancheS ? order.trancheS : "0x" + "0".repeat(64);
+  order.trancheB = order.trancheB ? order.trancheB : "0x" + "0".repeat(64);
+  order.transferDataS = order.transferDataS ? order.transferDataS : "0x";
+
+  await setOrderBalancesAndApprove(order);
+}
+
+/*
+ * we deployed some fake tokens: WETH, LRC, GTO.
+ * we can invoke setBalance method to set the token balance of an address.
+ */
+async function setOrderBalancesAndApprove(order) {
+  const sellToken = new web3.eth.Contract(JSON.parse(dummyTokenABI), order.tokenS);
+  const txData1 = sellToken.methods.setBalance(order.owner, numberToHex(order.amountS)).encodeABI();
+  await sendTransaction(order.owner, order.tokenS, 0, txData1, order.signerPrivateKey);
+
+  const feeToken = new web3.eth.Contract(JSON.parse(dummyTokenABI), order.feeToken);
+  let feeTokenAmount = order.feeAmount;
+  if (order.feeToken === order.tokenS) {
+    feeTokenAmount = order.feeAmount + order.amountS;
+  }
+  const txData2 = feeToken.methods.setBalance(order.owner, numberToHex(feeTokenAmount)).encodeABI();
+  await sendTransaction(order.owner, order.feeToken, 0, txData2, order.signerPrivateKey);
+
+  // do approval: approve a big number:
+  const approveTxData1 = sellToken.methods.approve(tradeDelegateAddress, numberToHex(1e+28)).encodeABI();
+  await sendTransaction(order.owner, order.tokenS, 0, approveTxData1, order.signerPrivateKey);
+
+  if (order.feeToken !== order.tokenS) {
+    const approveTxData2 = feeToken.methods.approve(tradeDelegateAddress, numberToHex(1e+28)).encodeABI();
+    await sendTransaction(order.owner, order.feeToken, 0, approveTxData2, order.signerPrivateKey);
+  }
+}
+
+function createRings() {
+  // you can create new account using webjs:
   // const order1Owner = web3.eth.accounts.create();
-  // const order1OwnerPrivateKey = order1Owner.privateKey.substr(2);
-  // here, we use account from ganache's account list.
-  const order1Owner = "0x8ea825a5fd77fd4180da0bb0c14309925c991b58";
-  const order1OwnerPrivateKey = "b6363ec295018ed93759777139049dbb098734843c311ebb9951c1e93feffcb4";
+  const order1Owner = "0xc0ff3f78529ab90f765406f7234ce0f2b1ed69ee";
+  const order1OwnerPrivateKey = "4c5496d2745fe9cc2e0aa3e1aad2b66cc792a716decf707ddb3f92bd2d93ad24";
 
-  const order2Owner = "0x201bba420af6cebdaa29b7aff1244447bab35074";
-  const order2OwnerPrivateKey = "3c3cb9b2fcab41e588d5aa0066928f855f2cf09e5c817fc41350eae9cfe8dc36";
+  const order2Owner = "0x611db73454c27e07281d2317aa088f9918321415";
+  const order2OwnerPrivateKey = "04b9e9d7c1385c581bab12600834f4f90c6e19142faae6c2de670bfb4b5a08c4";
 
-  // do authorize first. only need to do it once.
-  // await doAuthorize();
-
-  /* send some eth to order1Owner and order1Owner,
-   * so they can approve token to loopring tradeDelegate:
-   * 0.01 ETH should be enough
-   */
-  // await sendTransaction(miner, order1Owner, 0.01 * 1e18, "0x0", minerPrivateKey);
-  // await sendTransaction(miner, order2Owner, 0.01 * 1e18, "0x0", minerPrivateKey);
-
-  /*
-   * we deployed some fake tokens on kovan testnet,
-   * Any address can get these tokens by request the token contract's setBalance method:
-   */
-
-  const LrcToken = new web3.eth.Contract(JSON.parse(dummyTokenABI), LrcAddress);
-  const WETHToken = new web3.eth.Contract(JSON.parse(dummyTokenABI), WETHAddress);
-
-  // set LRC balance for order1Owner
-  const setBalanceTxData1 = LrcToken.methods.setBalance(order1Owner, numberToHex(1e22)).encodeABI();
-  await sendTransaction(order1Owner, LrcAddress, 0, setBalanceTxData1, order1OwnerPrivateKey);
-
-  // set WETH balance for order2Owner
-  const setBalanceTxData2 = WETHToken.methods.setBalance(order2Owner, numberToHex(10 * 1e18)).encodeABI();
-  await sendTransaction(order2Owner, WETHAddress, 0, setBalanceTxData2, order2OwnerPrivateKey);
-  // set LRC balance for order2Owner, so it can pay fee using lrc token.
-  const setBalanceTxData3 = LrcToken.methods.setBalance(order2Owner, numberToHex(100 * 1e18)).encodeABI();
-  await sendTransaction(order2Owner, LrcAddress, 0, setBalanceTxData3, order2OwnerPrivateKey);
-
-  // do approval to loopring trade delegate:
-  const approveTxData1 = LrcToken.methods.approve(tradeDelegateAddress, numberToHex(1e+22)).encodeABI();
-  await sendTransaction(order1Owner, LrcAddress, 0, approveTxData1, order1OwnerPrivateKey);
-
-  const approveTxData2 = LrcToken.methods.approve(tradeDelegateAddress, numberToHex(100 * 1e18)).encodeABI();
-  await sendTransaction(order2Owner, LrcAddress, 0, approveTxData2, order2OwnerPrivateKey);
-  const approveTxData3 = WETHToken.methods.approve(tradeDelegateAddress, numberToHex(10 * 1e18)).encodeABI();
-  await sendTransaction(order2Owner, WETHAddress, 0, approveTxData3, order2OwnerPrivateKey);
+  let validSince = (new Date()).getTime() / 1000; // system time seconds
+  validSince = Math.floor(validSince);
 
   /* create orders:
    * order1: buy 1000 LRC, sell 1 WETH
@@ -193,48 +257,96 @@ async function test() {
     version: 0,
     owner: order1Owner,
     signerPrivateKey: order1OwnerPrivateKey,
-    tokenS: "WETH",
-    tokenB: "LRC",
+    tokenS: WETHAddress,
+    tokenB: LrcAddress,
     amountS: 1e18,
     amountB: 1000e18,
+    validSince: validSince,
     feeToken: LrcAddress,
     feeAmount: 2e18,
+    signAlgorithm: pjs.SignAlgorithm.Ethereum,
   };
   const order2 = {
     version: 0,
     owner: order2Owner,
     signerPrivateKey: order2OwnerPrivateKey,
-    tokenS: "LRC",
-    tokenB: "WETH",
+    tokenS: LrcAddress,
+    tokenB: WETHAddress,
     amountS: 1000e18,
     amountB: 1e18,
+    validSince: validSince,
     feeToken: LrcAddress,
     feeAmount: 3e18,
+    signAlgorithm: pjs.SignAlgorithm.Ethereum,
   };
 
   // generate rings and sign:
-  const context = new pjs.Context(0, 0, tradeDelegateAddress, tradeHistoryAddress, brokerRegistryAddress, orderRegistryAddress, feeHolderAddress, orderBookAddress, burnRateTableAddress, LrcAddress, 1000, 0);
   const ringsInfo = {
     transactionOrigin: miner,
     miner: miner,
     rings: [[0, 1]],
     orders: [order1, order2],
   };
+
+  return ringsInfo;
+}
+
+async function test() {
+  // do authorize first. only need to do it once.
+  // await doAuthorize();
+
+  /* send some eth to order1Owner and order1Owner,
+   * so they can approve token to loopring tradeDelegate:
+   * 0.01 ETH should be enough
+   */
+  // await sendTransaction(miner, order1Owner, 0.01 * 1e18, "0x0", minerPrivateKey);
+  // await sendTransaction(miner, order2Owner, 0.01 * 1e18, "0x0", minerPrivateKey);
+
+
+  // const WETHToken = new web3.eth.Contract(JSON.parse(dummyTokenABI), WETHAddress);
+
+  // // set LRC balance for order1Owner
+  // const setBalanceTxData1 = LrcToken.methods.setBalance
+  // await sendTransaction(order1Owner, LrcAddress, 0, setBalanceTxData1, order1OwnerPrivateKey);
+
+  // // set WETH balance for order2Owner
+  // const setBalanceTxData2 = WETHToken.methods.setBalance(order2Owner, numberToHex(10 * 1e18)).encodeABI();
+  // await sendTransaction(order2Owner, WETHAddress, 0, setBalanceTxData2, order2OwnerPrivateKey);
+  // // set LRC balance for order2Owner, so it can pay fee using lrc token.
+  // const setBalanceTxData3 = LrcToken.methods.setBalance(order2Owner, numberToHex(100 * 1e18)).encodeABI();
+  // await sendTransaction(order2Owner, LrcAddress, 0, setBalanceTxData3, order2OwnerPrivateKey);
+
+  // // do approval to loopring trade delegate:
+  // const approveTxData1 = LrcToken.methods.approve(tradeDelegateAddress, numberToHex(1e+22)).encodeABI();
+  // await sendTransaction(order1Owner, LrcAddress, 0, approveTxData1, order1OwnerPrivateKey);
+
+  // const approveTxData2 = LrcToken.methods.approve(tradeDelegateAddress, numberToHex(100 * 1e18)).encodeABI();
+  // await sendTransaction(order2Owner, LrcAddress, 0, approveTxData2, order2OwnerPrivateKey);
+  // const approveTxData3 = WETHToken.methods.approve(tradeDelegateAddress, numberToHex(10 * 1e18)).encodeABI();
+  // await sendTransaction(order2Owner, WETHAddress, 0, approveTxData3, order2OwnerPrivateKey);
+
+  const ringsInfo = createRings();
+
+  for (const order of ringsInfo.orders) {
+    await setupOrder(order);
+  }
+
   const ringsGenerator = new pjs.RingsGenerator(context);
   await ringsGenerator.setupRingsAsync(ringsInfo); // sign orders, and ringsInfo.
 
   console.log("ringsInfo:", ringsInfo);
 
-  // // encode rings
-  // const bs = ringsGenerator.toSubmitableParam(ringsInfo);
+  // encode rings
+  const bs = ringsGenerator.toSubmitableParam(ringsInfo);
+  console.log("encoded param:", bs);
 
-  // // submit rings:
-  // const submitter = new web3.eth.Contract(JSON.parse(submitterABI), ringSubmitterAddress);
-  // const txData = submitter.methods.simulateAndReport(web3.utils.hexToBytes(bs), {from: ringsInfo.transactionOrigin}).encodeABI();
-  // await sendTransaction(miner, ringSubmitterAddress, 0, txData);
+  // submit rings:
+  const submitter = new web3.eth.Contract(JSON.parse(submitterABI), ringSubmitterAddress);
+  const txData = submitter.methods.submitRings(web3.utils.hexToBytes(bs)).encodeABI();
+  await sendTransaction(miner, ringSubmitterAddress, 0, txData, minerPrivateKey);
 
-  // // parse event:
-  // watchAndPrintEvent(submitter, "RingMined");
+  // parse event:
+  watchAndPrintEvent(submitter, "RingMined");
 
 }
 
